550e38225162600961d4af51c51f3e97903bd02b
malkiatsin <malkiat.singh@basware.com>
2021-08-01 16:26:00 +0530

    removed content

--- MyLittleKnowledge/Selenium/workspace/LittleKnowledgeWorkSpace/src/utility/BrowserFactory.java
+++ MyLittleKnowledge/Selenium/workspace/LittleKnowledgeWorkSpace/src/utility/BrowserFactory.java
@@ -1,9 +1,5 @@
 package utility;
 
-/**
- * @author Ai-Media
- *
- */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
